341ba5806b0c4b69882b45441c683439ffee069d
Thomas Salcedo <thomas.salcedo@esker.com>
2022-07-05 10:42:32 +0200

    Remove the commit link

--- dist/index.js
+++ dist/index.js
@@ -2467,12 +2467,6 @@ function createMessageCard(notificationTitle, notificationText, notificationColo
                 target: [`${repoUrl}/actions/runs/${runId}`],
                 '@type': 'ViewAction',
                 name: 'View Workflow Run'
-            },
-            {
-                '@context': 'http://schema.org',
-                target: [commit.data.html_url],
-                '@type': 'ViewAction',
-                name: 'View Commit Changes'
             }
         ]
     };
